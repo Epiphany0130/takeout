4ca8e2236c65b54189202c3513e22f9a1a8db8f7
Epiphany0130 <sdguyq@163.com>
2025-08-02 13:31:50 +0800

    启用禁用分类业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -76,4 +76,17 @@ public class CategoryController {
         categoryService.update(categoryDTO);
         return Result.success();
     }
+
+    /**
+     * 启用禁用分类
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用分类")
+    public Result<String> startOrStop(@PathVariable("status") Integer status, Long id) {
+        categoryService.startOrStop(status, id);
+        return Result.success();
+    }
 }
